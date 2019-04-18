ed7a7248b759ff6936494c1eb0c98293d2223ad1
zhuyong <zhuyong@handlecar.com>
2019-04-18 10:39:36 +0800

    初始化facing

---
+++ src/main/java/com/zhy/facing/FacingApplication.java
@@ -0,0 +1,13 @@
+package com.zhy.facing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FacingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FacingApplication.class, args);
+    }
+
+}
